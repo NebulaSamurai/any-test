6d30095d7bd2291c2e5b7a04b57a6048172f436d
Max-Sharko <ninja.nebula.any@gmail.com>
2024-08-15 11:17:10 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>MAIN</p>
-        <p>MAIN</p>
-        <p>MAIN</p>
-        <p>MAIN</p>
+        <p>MAIN second commit</p>
       </header>
     </div>
   );
